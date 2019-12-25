1aedb8cd1b3075960fced9cef574dd4b09689eee
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-12-25 20:30:57 +0300

    implemented onPush strategy

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,17 +1,26 @@
-import { Component, Inject, OnDestroy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnDestroy
+} from '@angular/core';
+import { HttpEventType } from '@angular/common/http';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { UploadFileStatus } from './../../classes/upload-file-status';
+import { duration } from '@firestitch/date';
+
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+
 import { UploadService } from '../../services/upload.service';
-import { duration } from '@firestitch/date';
-import { UploadStatus } from 'src/app/classes/upload-status';
-import { HttpEventType } from '@angular/common/http';
+import { UploadStatus } from '../../classes/upload-status';
+import { UploadFileStatus } from '../../classes/upload-file-status';
 
 @Component({
   selector: 'fs-component',
   templateUrl: 'upload.component.html',
   styleUrls: ['upload.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FsUploadComponent implements OnDestroy {
 
@@ -36,40 +45,21 @@ export class FsUploadComponent implements OnDestroy {
 
   private _destroy$ = new Subject<void>();
 
-  constructor(public dialogRef: MatDialogRef<FsUploadComponent>,
-              private _uploadService: UploadService,
-              @Inject(MAT_DIALOG_DATA) public data) {
+  constructor(
+    public dialogRef: MatDialogRef<FsUploadComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private _uploadService: UploadService,
+    private _cdRef: ChangeDetectorRef,
+  ) {
 
     this._uploadService.uploadStatus$
       .pipe(
         takeUntil(this._destroy$),
       )
       .subscribe((uploadedStatuses: UploadStatus[]) => {
+        this._calcRemaning(uploadedStatuses);
 
-        this.remaining = '';
-
-        if (uploadedStatuses.length) {
-
-          let bytesPerSecond = 0;
-          let remainingBytes = 0;
-
-          const statuses = uploadedStatuses.filter(item => {
-            return item.status === HttpEventType.UploadProgress;
-          });
-
-          statuses.forEach(uploadedStatus => {
-            bytesPerSecond += uploadedStatus.bytesPerSecond;
-            remainingBytes += uploadedStatus.remainingBytes;
-          });
-
-          const remainingSeconds = remainingBytes / (bytesPerSecond / statuses.length);
-
-          if (remainingSeconds > 0) {
-            this.remaining = ' '
-                              .concat(duration(remainingSeconds, { hours: true, minutes: true, seconds: true }))
-                              .concat(' remaining');
-          }
-        }
+        this._cdRef.markForCheck();
       });
 
     this.data.files
@@ -77,58 +67,14 @@ export class FsUploadComponent implements OnDestroy {
         takeUntil(this._destroy$),
       )
       .subscribe(files => {
-
         this.files.push(...files);
         this.clearClosing();
 
         files.forEach(file => {
-
-          file.statusSubject
-          .pipe(
-            takeUntil(this._destroy$),
-          )
-          .subscribe(status => {
-
-            let uploading = 0;
-            let uploaded = 0;
-            let processing = 0;
-            let failed = 0;
-            let cancelled = 0;
-
-            this.files.forEach(file => {
-              switch (file.status) {
-                case UploadFileStatus.Uploading:
-                  uploading++;
-                  break;
-                case UploadFileStatus.Processing:
-                  processing++;
-                  break;
-                case UploadFileStatus.Uploaded:
-                  uploaded++;
-                  break;
-                case UploadFileStatus.Failed:
-                  failed++;
-                  break;
-                case UploadFileStatus.Cancelled:
-                  cancelled++;
-                  break;
-              }
-            });
-
-            this.uploading = uploading;
-            this.uploaded = uploaded;
-            this.processing = processing;
-            this.failed = failed;
-            this.cancelled = cancelled;
-
-            if (status !== UploadFileStatus.Failed &&
-                status !== UploadFileStatus.Cancelled &&
-                !this.uploading &&
-                !this.processing) {
-              this.startClosing();
-            }
-          });
+          this._processFile(file);
         });
+
+        this._cdRef.markForCheck();
     });
   }
 
@@ -146,6 +92,81 @@ export class FsUploadComponent implements OnDestroy {
     this.closingPercent = 0;
   }
 
+  protected _calcRemaning(uploadedStatuses: UploadStatus[]) {
+    this.remaining = '';
+
+    if (uploadedStatuses.length) {
+
+      let bytesPerSecond = 0;
+      let remainingBytes = 0;
+
+      const statuses = uploadedStatuses.filter(item => {
+        return item.status === HttpEventType.UploadProgress;
+      });
+
+      statuses.forEach(uploadedStatus => {
+        bytesPerSecond += uploadedStatus.bytesPerSecond;
+        remainingBytes += uploadedStatus.remainingBytes;
+      });
+
+      const remainingSeconds = remainingBytes / (bytesPerSecond / statuses.length);
+
+      if (remainingSeconds > 0) {
+        this.remaining = ' '
+          .concat(duration(remainingSeconds, { hours: true, minutes: true, seconds: true }))
+          .concat(' remaining');
+      }
+    }
+  }
+
+  protected _processFile(file) {
+    file.statusSubject
+      .pipe(
+        takeUntil(this._destroy$),
+      )
+      .subscribe(status => {
+
+        let uploading = 0;
+        let uploaded = 0;
+        let processing = 0;
+        let failed = 0;
+        let cancelled = 0;
+
+        this.files.forEach(f => {
+          switch (f.status) {
+            case UploadFileStatus.Uploading:
+              uploading++;
+              break;
+            case UploadFileStatus.Processing:
+              processing++;
+              break;
+            case UploadFileStatus.Uploaded:
+              uploaded++;
+              break;
+            case UploadFileStatus.Failed:
+              failed++;
+              break;
+            case UploadFileStatus.Cancelled:
+              cancelled++;
+              break;
+          }
+        });
+
+        this.uploading = uploading;
+        this.uploaded = uploaded;
+        this.processing = processing;
+        this.failed = failed;
+        this.cancelled = cancelled;
+
+        if (status !== UploadFileStatus.Failed &&
+          status !== UploadFileStatus.Cancelled &&
+          !this.uploading &&
+          !this.processing) {
+          this.startClosing();
+        }
+      });
+  }
+
   private startClosing() {
 
     if (this.failed) {
@@ -154,9 +175,12 @@ export class FsUploadComponent implements OnDestroy {
 
     const interval = 100;
     this.clearClosing();
+
     this.closingProgressInterval = setInterval(() => {
       this.closingPercent += (interval / (this.closingTimeout * 1000)) * 100;
       this.closingSeconds = Math.floor(this.closingTimeout - ((this.closingPercent / 100) * this.closingTimeout));
+
+      this._cdRef.markForCheck();
 
       if (this.closingSeconds <= 0) {
         this.clearClosing();
